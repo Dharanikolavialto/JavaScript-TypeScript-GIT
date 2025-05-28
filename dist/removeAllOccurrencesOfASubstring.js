2a80c9a992a6ee7f7d3badfbae390aefa23d04e9
Dharanikolavialto <dharani.kola@vialto.com>
2025-05-28 20:01:13 +0530

    Delete dist/removeAllOccurrencesOfASubstring.js

--- dist/removeAllOccurrencesOfASubstring.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function removeOccurrences(s, part) {
-    while (s.includes(part)) {
-        s = s.replace(part, '');
-    }
-    return s;
-}
-console.log(removeOccurrences("daabcbaabcbc", "abc")); // Output: "dab"
-console.log(removeOccurrences("axxxxyyyyb", "xy")); // Output: "ab"
