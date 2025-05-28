94bfdc823158c009dc0619ed866d98dc4cd4ea4c
Dharanikolavialto <dharani.kola@vialto.com>
2025-05-28 20:00:57 +0530

    Delete src/removeAllOccurrencesOfASubstring.ts

--- src/removeAllOccurrencesOfASubstring.ts
+++
@@ -1,8 +0,0 @@
-function removeOccurrences(s: string, part: string): string {
-    while (s.includes(part)) {
-        s = s.replace(part, '');
-    }
-    return s;
-}
-console.log(removeOccurrences("daabcbaabcbc", "abc")); // Output: "dab"
-console.log(removeOccurrences("axxxxyyyyb", "xy"));    // Output: "ab"
