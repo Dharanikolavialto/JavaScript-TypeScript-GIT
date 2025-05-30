f016bc31cf6f49bfc4821d0215533815f99a2aff
DharaniKola <dharani.kola@vialto.com>
2025-05-30 19:13:41 +0530

    Removing Starts and Longest Continuous Substring

---
+++ dist/removingStars.js
@@ -0,0 +1,21 @@
+"use strict";
+function removeStars(s) {
+    const ans = []; // Use a stack (array) to build the final string
+    // Loop through each character in the string
+    for (const code of s) {
+        if (code === '*') {
+            // When we find a star, remove the last added character from the stack
+            ans.pop();
+        }
+        else {
+            // If it's a normal character, add it to the stack
+            ans.push(code);
+        }
+    }
+    // Joining all characters in the stack to form the final string
+    return ans.join('');
+}
+// inputs
+console.log(removeStars("leet**cod*e")); // "lecoe"
+console.log(removeStars("erase*****")); // ""
+console.log(removeStars("erase*")); // eras
