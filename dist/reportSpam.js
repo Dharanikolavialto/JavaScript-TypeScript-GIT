413276fd582fca451e29b270d29ebc9001809123
DharaniKola <dharani.kola@vialto.com>
2025-06-04 20:59:33 +0530

    Report Spam Message

---
+++ dist/reportSpam.js
@@ -0,0 +1,17 @@
+"use strict";
+// 3295. Report Spam Message
+function spamReport(message, bannedWords) {
+    const bannedSet = new Set(bannedWords);
+    let bannedCount = 0;
+    for (const word of message) {
+        if (bannedSet.has(word)) {
+            bannedCount++;
+            if (bannedCount >= 2)
+                return true;
+        }
+    }
+    return false;
+}
+// inputs
+console.log(spamReport(["hello", "world", "leetcode"], ["world", "hello"])); // true
+console.log(spamReport(["hello", "programming", "fun"], ["world", "programming", "leetcode"])); // false
