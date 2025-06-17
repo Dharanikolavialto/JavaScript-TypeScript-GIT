40e9275f16ffafeb03459ebe2600aef142eebd5d
DharaniKola <dharani.kola@vialto.com>
2025-06-17 20:05:51 +0530

    Palindromic Substrings

---
+++ src/palindromicSubstrings.ts
@@ -0,0 +1,30 @@
+// 647. Palindromic Substrings
+
+function palindromeSubstrings(s: string): number {
+    let count= 0;
+    for (let i = 0; i < s.length; i++) {
+        // Odd length palidromes
+        count += expandAroundCenter(s, i, i);
+
+        // Even length palidromes
+        count += expandAroundCenter(s, i, i+1);
+    }
+    return count;
+
+    function expandAroundCenter(s: string, left: number, right: number): number {
+        let count= 0;
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+}
+
+// inputs
+console.log(palindromeSubstrings("abc"));           // 3
+console.log(palindromeSubstrings("aaa"));          // 6
+console.log(palindromeSubstrings("abba"));        // 6
+console.log(palindromeSubstrings("racecar"));    // 10
+console.log(palindromeSubstrings(""));          // 0  
